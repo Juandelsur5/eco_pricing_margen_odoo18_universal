35fbf1a172f5b694cfca6b97876d41ce67203bf0
Juandel Sur <tu_correo_github@gmail.com>
2026-04-03 15:26:06 -0500

    fix definitivo forzar precio directo

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,10 +4,15 @@ from odoo import models, api
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.depends('product_id', 'product_uom_qty', 'order_id.pricelist_id')
-    def _compute_price_unit(self):
-        super()._compute_price_unit()
+    @api.onchange('product_id')
+    def _onchange_product_id_force_price(self):
+        self._force_price()
 
+    @api.onchange('order_id.pricelist_id')
+    def _onchange_pricelist_force_price(self):
+        self._force_price()
+
+    def _force_price(self):
         for line in self:
             product = line.product_id
             pricelist = line.order_id.pricelist_id
@@ -26,3 +31,14 @@ class SaleOrderLine(models.Model):
                 line.price_unit = tmpl.x_final_price_mayorista
             elif "OFERTA" in name:
                 line.price_unit = tmpl.x_final_price_oferta
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super().create(vals_list)
+        lines._force_price()
+        return lines
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._force_price()
+        return res

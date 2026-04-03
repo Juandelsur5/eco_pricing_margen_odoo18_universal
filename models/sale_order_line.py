0cf5496fd8b2d9ee371bdc2a16a86a6ffec99292
Juandel Sur <tu_correo_github@gmail.com>
2026-04-03 15:28:09 -0500

    fix final create override pricing

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -4,16 +4,11 @@ from odoo import models, api
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.onchange('product_id')
-    def _onchange_product_id_force_price(self):
-        self._force_price()
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super().create(vals_list)
 
-    @api.onchange('order_id.pricelist_id')
-    def _onchange_pricelist_force_price(self):
-        self._force_price()
-
-    def _force_price(self):
-        for line in self:
+        for line in lines:
             product = line.product_id
             pricelist = line.order_id.pricelist_id
 
@@ -32,13 +27,4 @@ class SaleOrderLine(models.Model):
             elif "OFERTA" in name:
                 line.price_unit = tmpl.x_final_price_oferta
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        lines = super().create(vals_list)
-        lines._force_price()
         return lines
-
-    def write(self, vals):
-        res = super().write(vals)
-        self._force_price()
-        return res
